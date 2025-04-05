91da3fb2267dd0fe5ceade454938147577b16c18
Ian-Mata <imata4@ucmerced.edu>
2025-04-04 20:33:36 -0700

    Reapply "Uploaded backend"
    This reverts commit 54342e2ba960ae12c1a761f9d8184915440272c6.

---
+++ backend/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
+import json
+import os.path
+
+app = Flask(__name__)
+CORS(app)
+
+#This defines the default root
+@app.route("/")
+def uploadPage():
+    return render_template('upload.html')
+    
+@app.route("/connect", methods=['GET'])
+def connectToPage():
+	
+	#Put code here that checks if api file is here!
+	
+	#Prints out a simple boolean if the file does not exist!
+	fileExists = os.path.isfile("apikey.txt")
+	
+	if(fileExists is False):
+		response = jsonify({'keyFound': 0}) #No Key was found!
+	else:
+		#Add code to see if file is populated!
+		response = jsonify({'keyFound': 1}) #Key was found!
+	#response.headers.add("Access-Control-Allow-Origin", "*")
+	
+	#return render_template('upload.html')
+	
+	return response, 200
+	
+@app.route("/createAPIKey", methods=['POST'])
+def createKey():
+	
+    
+
+    
+    
+if __name__ == '__main__':
+	app.run()
